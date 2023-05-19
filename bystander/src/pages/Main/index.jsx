11fa1cbc4634b28f60f26dd468efb0d3b533330e
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-05-19 13:36:30 +0900

    추가:매안매뉴(30%)

--- bystander/src/pages/Main/index.jsx
+++ bystander/src/pages/Main/index.jsx
@@ -5,6 +5,8 @@ function Main() {
     return (
         <S.Container>
             <S.Title>BYSTANDER</S.Title>
+            <p>당신은 이제부터 학교폭력의 방관자가 되게됩니다.</p>
+            <p>당신의 이름을 작성해주세요.</p>
         </S.Container>
     );
 }
--- bystander/src/pages/Main/styled.js
+++ bystander/src/pages/Main/styled.js
@@ -3,9 +3,20 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+
+  p {
+    font-size: 2rem;
+  }
 `;
 
-export const Title = styled.div`
+export const TextBox = styled.div``;
+
+export const Title = styled.h1`
   font-size: 10rem;
   font-weight: bold;
 `;
