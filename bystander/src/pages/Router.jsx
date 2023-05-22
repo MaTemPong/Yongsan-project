2bd25a6bbce14d244571d3235f24259af8798b03
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-05-22 09:30:57 +0900

    추가:SNS, Header폴더 제작

--- bystander/src/pages/Router.jsx
+++ bystander/src/pages/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 
-function Router() {
+function Router() { 
     return (
         <BrowserRouter>
             <Routes>
