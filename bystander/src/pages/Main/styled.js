afe0ec2267818b63093a880b4e80f6d27c2efeeb
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-22 09:36:06 +0900

    수정:  스타일 수정

--- bystander/src/pages/Main/styled.js
+++ bystander/src/pages/Main/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import schoolImg from "../../media/img/school.jpg"
+import schoolImg from "../../media/img/school.jpg";
 
 export const Container = styled.div`
   width: 100vw;
@@ -12,41 +12,49 @@ export const Container = styled.div`
   white-space: nowrap;
   text-align: center;
   font-size: 1.5rem;
-  box-shadow: inset 1px 1px 50px 10px black;  
-  font-family: 'Jeonju_gakR';
+  box-shadow: inset 1px 1px 50px 10px black;
+  font-family: "Jeonju_gakR";
 `;
 
 export const InputSet = styled.div`
-  width: 250px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 300px;
   height: 50px;
-  display:flex;
-  border-radius: 100px;
-  overflow:hidden;
-  border: 2px solid #333;
-  input{
+  display: flex;
+  border-radius: 9px;
+  overflow: hidden;
+  border: 0.1px solid #333;
+  input {
+    position: absolute;
+    left: 30px;
     width: 70%;
     height: 100%;
-    font-size: 1.5rem;
-    outline:none;
+    font-size: 20px;
+    outline: none;
     padding: 15px;
-  } 
-  button{
-    width: 30%;
+    border: 0;
+  }
+  button {
+    position: absolute;
+    right: 0;
+    width: 75px;
     height: 100%;
-    background:  #333;
+    background: #333;
     color: white;
-    font-weight:bold;
-    font-size: 1.5rem;  
+    font-weight: 700;
+    font-size: 18px;
   }
 `;
 
-export const Title = styled.h1`
-`;
+export const Title = styled.h1``;
 
 export const Description = styled.p`
- line-height: 40px;
- color: gray; 
+  line-height: 40px;
+  color: gray;
 `;
 
 export const MainContent = styled.div`
-animation: fadeInUp2 3s;`;
+  animation: fadeInUp2 3s;
+`;
